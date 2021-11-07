17379173f6fea8f7de8e2ad19f7f5b01e8b29ed8
raptogit <rohansolanke4@gmail.com>
2021-11-07 09:27:19 +0530

    date deleted

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const date = require(__dirname + "/date.js")
 const mongoose=require("mongoose")
 const app = express()
-mongoose.connect("")
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
 app.set('view engine', 'ejs');
+const itemsSchema ={
+    name: String,
+  };
+const Item=mongoose.model("Item",itemsSchema);
+const item1=new Item({
+    name:"Welcome to your todolist"
+});
+const item2=new Item({
+    name:"Use + sign to add your task !"
+});
+const item3=new Item({
+    name:"Click on the item if you finish your task"
+});
+
+const defaultItems=[item1,item2,item3];
+Item.insertMany(defaultItems,function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Success");
+    }
+    
+})
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.get("/", function (req, res) {
-    day=date();
-    res.render("list", { listTitle: day, newItem: items });
+    
+    res.render("list", { listTitle: "Today", newItem: items });
 })
 
 app.post("/", function (req, res) {
